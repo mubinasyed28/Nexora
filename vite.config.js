69fb3c8e7dc3d38c89ec3ec56234d5cf49830e75
Mubina <mubina282006@gmail.com>
2025-03-25 11:40:20 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy : {
-      "/api" : "http://localhost:8000"
+    proxy: {
+      "/api": "http://localhost:8000"
     },
   },
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  resolve: {
+    extensions: ['.js', '.jsx'], // Allow .jsx without specifying extension
+  },
+  plugins: [react()],
 })
