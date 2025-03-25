8083b058ccd5eefe98d28db11b2d046b128ced6d
Mubina <mubina282006@gmail.com>
2025-03-25 15:01:56 +0530

    ...

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import { errorHandler } from './middlewares/errorHandler.middleware.js';
 const app = express();
 
 app.use(cors({
-    origin: "https://nexora-2.vercel.app/",
+    origin: "https://nexora-3.vercel.app/",
     credentials: true
 }))
 
